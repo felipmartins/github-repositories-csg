12e28b271590d0b05e43028d53dfc534e5ee72c1
felipmartins <felipmartins94@gmail.com>
2024-01-31 10:13:31 -0300

    del: remove sanity endpoint

--- src/app.py
+++ src/app.py
@@ -4,8 +4,3 @@ from src.controllers.user_controller import router as user_router
 app = FastAPI()
 
 app.include_router(user_router, tags=["users"])
-
-
-@app.get("/health")
-async def sanity_check():
-    return {"message": "Hello World"}
