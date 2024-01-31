912c661a8ee09268e0b0192154b33c90f82d0f06
felipmartins <felipmartins94@gmail.com>
2024-01-31 10:38:21 -0300

    config: add celery config data

--- src/config.py
+++ src/config.py
@@ -5,3 +5,9 @@ APP_CONFIG = {
     "MONGO_URI": os.getenv("MONGO_URI", "mongodb://localhost:27017"),
     "DB_NAME": os.getenv("DB_NAME", "csgi_test"),
 }
+
+
+CELERY_CONFIG = {
+    "broker": os.getenv("CELERY_BROKER_URL", "pyamqp://guest@localhost//"),
+    "backend": os.getenv("CELERY_RESULT_BACKEND", "mongodb://localhost:27017"),
+}
