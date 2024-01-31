79199c78867b076142d904bcaefd2f95a601a1fd
felipmartins <felipmartins94@gmail.com>
2024-01-31 10:13:02 -0300

    feat: include user controller router to app

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
-
+from src.controllers.user_controller import router as user_router
 
 app = FastAPI()
+
+app.include_router(user_router, tags=["users"])
 
 
 @app.get("/health")
