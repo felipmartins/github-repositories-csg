c180b27fcd83cd9493d764e4673697f9162f2157
felipmartins <felipmartins94@gmail.com>
2024-01-31 10:20:25 -0300

    refact: implement the use of templates

--- src/controllers/user_controller.py
+++ src/controllers/user_controller.py
@@ -1,25 +1,43 @@
-from fastapi import APIRouter, status, Form
+from fastapi import APIRouter, status, Form, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 from src.config import APP_CONFIG
 from src.repositories.user_repository import UserRepository
 from src.github_api import get_repo_data
+from starlette.responses import RedirectResponse
 from typing import Annotated
 
 router = APIRouter()
 
 
-@router.get("/", status_code=status.HTTP_200_OK)
-def get_users():
-    return [user.to_dict() for user in UserRepository(APP_CONFIG).find_all()]
+templates = Jinja2Templates(directory="src/templates")
+
+
+@router.get("/", status_code=status.HTTP_200_OK, response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request},
+    )
 
 
 @router.get("/{name}", status_code=status.HTTP_200_OK)
-def get_user(name: str):
+def get_user(request: Request, name: str):
     user = UserRepository(APP_CONFIG).find_by_name(name)
 
     if not user:
-        return {"message": "User not found"}, status.HTTP_404_NOT_FOUND
+        return RedirectResponse(
+            "/",
+            status_code=status.HTTP_301_MOVED_PERMANENTLY,
+        )
 
-    return user
+    return templates.TemplateResponse(
+        "repos.html",
+        {
+            "request": request,
+            "user": user,
+        },
+    )
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
@@ -29,4 +47,9 @@ def create_user(name: Annotated[str, Form()]):
         "repos": get_repo_data(name),
     }
 
-    return UserRepository(APP_CONFIG).insert_or_update(name, data)
+    user = UserRepository(APP_CONFIG).insert_or_update(name, data)
+
+    return RedirectResponse(
+        f"/{user.name}",
+        status_code=status.HTTP_301_MOVED_PERMANENTLY,
+    )
